fe93877394f0fbf127db176965f17c6cbfd55b01
SafayetHossainJoy <safayethossain.cse@gmail.com>
2024-06-08 18:34:18 +0600

    Last  commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hrms/Homepage.dart';
+// import 'package:hrms/Homepage.dart';
 import 'package:hrms/splash.dart';
 
 void main() {
@@ -11,11 +11,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-       //home: SplashScreen(),
+       home: SplashScreen(),
       // home: ExpensePage(),
       // home: AccessTokenPage(),
          // home: TotalDaysPerMonthCard(),
-home: HomePage(),
+       //home: HomePage(),
     );
   }
 }
